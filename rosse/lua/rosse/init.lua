f35112706aa8ed0d96035bb0ee9d6ca1c0382ce9
zukunftsdeuter <zukunftsdeuter@gmail.com>
2026-02-28 18:29:06 -0300

    Update init.lua

--- rosse/lua/rosse/init.lua
+++ rosse/lua/rosse/init.lua
@@ -1,1 +1,34 @@
+local M = {}
 
+M.config = {
+    undercurl = true,
+    transparent_mode = false,
+}
+
+function M.setup(opts)
+    M.config = vim.tbl_deep_extend("force", M.config, opts or {})
+end
+
+function M.load()
+    vim.cmd("hi clear")
+    if vim.fn.exists("syntax_on") then
+        vim.cmd("syntax reset")
+    end
+    vim.g.colors_name = "rosse"
+
+    -- 1. Cargar la paleta (aquí irían tus colores HEX)
+    local colors = require("rosse.palette") 
+    
+    -- 2. Aplicar transparencia si M.config.transparent_mode es true
+    if M.config.transparent_mode then
+        colors.bg0 = "NONE"
+    end
+
+    -- 3. Cargar y aplicar los grupos de resaltado (highlights)
+    local groups = require("rosse.groups").setup(colors, M.config)
+    for group, settings in pairs(groups) do
+        vim.api.nvim_set_hl(0, group, settings)
+    end
+end
+
+return M
